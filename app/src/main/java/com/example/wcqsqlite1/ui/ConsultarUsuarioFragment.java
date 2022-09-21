24922e980002b5055486ac8e1232d27d4657c2d1
wilCahuaya <48217068@continental.edu.pe>
2022-09-21 15:21:14 -0500

    Consultar usuario fragment

--- app/src/main/java/com/example/wcqsqlite1/ui/ConsultarUsuarioFragment.java
+++ app/src/main/java/com/example/wcqsqlite1/ui/ConsultarUsuarioFragment.java
@@ -1,36 +1,45 @@
-package com.example.wcqsqlite1;
-
-import androidx.appcompat.app.AppCompatActivity;
+package com.example.wcqsqlite1.ui;
 
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.wcqsqlite1.Entidades.SqliteUtil;
+import com.example.wcqsqlite1.R;
 
 import Utilidades.UtilWCQ;
 
-public class ConsultarUsuario extends AppCompatActivity {
+public class ConsultarUsuarioFragment extends Fragment {
     EditText edtConsulIdUsuario;
     TextView txtNombre, txtTelefono;
     Button btnBuscar;
     SqliteUtil conexion;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_consultar_usuario);
+    }
 
-        edtConsulIdUsuario= findViewById(R.id.edtConsulIdUsuario);
-        txtNombre= findViewById(R.id.txtNombre);
-        txtTelefono= findViewById(R.id.txtTelefono);
-        btnBuscar= findViewById(R.id.btnBuscar);
-        conexion= new SqliteUtil(this, UtilWCQ.BASE_DATOS, null,1);
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View vista=inflater.inflate(R.layout.fragment_consultar_usuario, container, false);
+        edtConsulIdUsuario= vista.findViewById(R.id.edtConsulIdUsuario);
+        txtNombre= vista.findViewById(R.id.txtNombre);
+        txtTelefono= vista.findViewById(R.id.txtTelefono);
+        btnBuscar= vista.findViewById(R.id.btnBuscar);
+        conexion= new SqliteUtil(getContext(), UtilWCQ.BASE_DATOS, null,1);
 
         btnBuscar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -38,16 +47,14 @@ public class ConsultarUsuario extends AppCompatActivity {
                 consultarUsuario();
             }
         });
-
-
+        return vista;
     }
-
     private void consultarUsuario() {
         SQLiteDatabase db = conexion.getReadableDatabase();
         //no mesclar SQL con logica de negocio
         String[] parametros={edtConsulIdUsuario.getText().toString()};
         String[] lista_de_campos={UtilWCQ.CAMPO_ID,UtilWCQ.CAMPO_NOMBRE,UtilWCQ.CAMPO_TELEFONO};
-        Toast.makeText(this, ""+parametros, Toast.LENGTH_SHORT).show();
+        Toast.makeText(getContext(), ""+parametros, Toast.LENGTH_SHORT).show();
         //captura en caso de error
         try {
             Cursor cursor=db.query(UtilWCQ.TABLA_USUARIO,lista_de_campos,UtilWCQ.CAMPO_ID+"=?",parametros,null,null,null);
@@ -56,7 +63,7 @@ public class ConsultarUsuario extends AppCompatActivity {
             txtTelefono.setText(cursor.getString(2));
             cursor.close();
         }catch (Exception e){
-            Toast.makeText(this, "Error al buscar Id USUARIO"+edtConsulIdUsuario.getText().toString(), Toast.LENGTH_SHORT).show();
+            Toast.makeText(getContext(), "Error al buscar Id USUARIO"+edtConsulIdUsuario.getText().toString(), Toast.LENGTH_SHORT).show();
         }
     }
 }
